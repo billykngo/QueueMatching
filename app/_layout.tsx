1471b785a9efaeb2609898c30702c972133c5231
Billy Ngo <shinlai@Billys-MacBook-Pro.local>
2024-08-13 19:20:11 -0500

    add 2 buttons and background color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router";
+import React from 'react';
+import HomePage from './index.js';
+//import AboutPage from './pages/AboutPage';
+//import NotFoundPage from './pages/NotFoundPage';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen 
+        name="index"
+        options={{
+          title: 'Queue Matching', // Set the title to what you want
+          headerStyle: { backgroundColor: 'black' }, // Customize the header background
+          headerTintColor: 'white', // Customize the text color /> 
+
+          }}/>
+    </Stack>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,49 @@
+import { Text, View, SafeAreaView, Button, StyleSheet } from "react-native";
+
+/*{flex: 1,
+justifyContent: "center",
+alignItems: "center", }*/
+export default function index() {
+  return (
+    <SafeAreaView style = {backgroundStyle.container}>
+      <Text color = 'white'> 
+        Edit app/Queue Matching.tsx to edit this screen. I want to see if this will go over the border of the app.
+      </Text>
+      <Button
+        color = "green"
+        title = "Team Name" 
+        onPress={() => console.log("Team Name pressed")}
+      />
+      <Button
+        color = "blue"
+        title = "Button 2" 
+        onPress={() => console.log("Team Name pressed")} />
+    </SafeAreaView>
+    );
+}
+
+  const backgroundStyle = StyleSheet.create ({
+    container: {
+      flex: 1, 
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#960C22",
+      textShadowColor: "#FFFFFF"
+    },
+
+  });
+  const teamStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "flex-start",
+      alignItems: "center", 
+    },
+  });
+  const restStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "right", 
+    },
+  });
+  
